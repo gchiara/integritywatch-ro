68fc413ffbba9a41f077bc17963f56277f361632
GChiara <girardelli.chiara@gmail.com>
2023-03-22 02:38:16 +0100

    Updated project files

--- public/header.php
+++ public/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id="iw-nav">
-  <a class="navbar-brand" href="https://transparency.de/" target="_blank"><img src="./images/logo_transparency_de.svg" alt="" /> </a>
+  <a class="navbar-brand" href="https://transparency.ro/" target="_blank"><img src="./images/ti_logo.png" alt="" /> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
